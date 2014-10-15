53d27c688688752d5d7290a82c6865b2aed6fb2d
Shota Oda <odendenden0606@gmail.com>
2014-10-16 01:07:44 +0900

    Login Form

---
+++ gyoumuApp1/PerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PerViewController.h
+//  gyoumuApp1
+//
+//  Created by Shota Oda on 2014/10/16.
+//  Copyright (c) 2014年 shota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerViewController : UIViewController
+
+@end
---
+++ gyoumuApp1/personalViewController.h
@@ -0,0 +1,13 @@
+//
+//  personalViewController.h
+//  gyoumuApp1
+//
+//  Created by Shota Oda on 2014/10/15.
+//  Copyright (c) 2014年 shota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface personalViewController : UIViewController
+
+@end
