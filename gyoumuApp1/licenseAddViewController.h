34d48b7910ffeb8f14caa70b117d1759a294aa28
Kouta Nishiura <plain0903@gmail.com>
2014-11-02 01:47:23 +0900

    ライセンス登録画面にボタンを追加

---
+++ gyoumuApp1/licenseAddViewController.h
@@ -0,0 +1,14 @@
+//
+//  licenseAddViewController.h
+//  gyoumuApp1
+//
+//  Created by FUNAICT201312 on 2014/11/02.
+//  Copyright (c) 2014年 shota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface licenseAddViewController : UIViewController
+- (IBAction)licenseAddBtn:(UIButton *)sender;
+
+@end
