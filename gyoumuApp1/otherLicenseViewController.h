5b7264d6c390f6c619a331347e36b0a522c2d759
Shota Oda <odendenden0606@gmail.com>
2014-11-20 17:42:49 +0900

    ログインユーザの研究室のライセンス情報の仮表示を実装 #75 licenseVCを削除し、licenseTableVCを作成した(MSの配置はotherLicenseVCをコピーした) NSUserDeaultsでログインユーザのlabCodeを参照してライセンス一覧を表示できるようにした 現在はは"badge"で識別して新しいものを上から表示するようになっています

--- gyoumuApp1/otherLicenseViewController.h
+++ gyoumuApp1/otherLicenseViewController.h
@@ -2,12 +2,12 @@
 //  otherLicenseViewController.h
 //  gyoumuApp1
 //
-//  Created by SegawaTakamasa on 2014/11/15.
+//  Created by Shota Oda on 2014/11/16.
 //  Copyright (c) 2014年 shota. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface otherLicenseViewController : UIViewController
+@interface otherLicenseViewController : UITableViewController
 
 @end
