a2b862ff1bcd4c50ab44f9397587053ab006f10e
Shota Oda <odendenden0606@gmail.com>
2014-11-21 22:19:25 +0900

    ライセンス一覧のレイアウトの変更1

--- gyoumuApp1/otherLicenseViewController2.m
+++ gyoumuApp1/otherLicenseViewController2.m
@@ -61,25 +61,40 @@
     return 1;
 }
 
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 60.0f;
+}
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    UIView *containerView = [[UIView alloc] init];
+    
+    UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 200)];
+    containerView.backgroundColor = [UIColor whiteColor];
     UILabel *maker = [[UILabel alloc] initWithFrame:CGRectMake(30, 10, 100, 20)];
     maker.text = @"メーカー";
     UILabel *software = [[UILabel alloc] initWithFrame:CGRectMake(maker.frame.origin.x+maker.frame.size.width+40, 10, 200, 20)];
     software.text = @"ソフトウェア";
-    UILabel *version = [[UILabel alloc] initWithFrame:CGRectMake(software.frame.origin.x+software.frame.size.width+40, 10, 200, 20)];
+    UILabel *version = [[UILabel alloc] initWithFrame:CGRectMake(software.frame.origin.x+software.frame.size.width+5, 10, 100, 20)];
     version.text = @"バージョン";
-    UILabel *own = [[UILabel alloc] initWithFrame:CGRectMake(version.frame.origin.x+version.frame.size.width+40, 10, 150, 20)];
+    UILabel *own = [[UILabel alloc] initWithFrame:CGRectMake(version.frame.origin.x+version.frame.size.width+50, 10, 50, 20)];
     own.text = @"保管";
-    UILabel *rest = [[UILabel alloc] initWithFrame:CGRectMake(own.frame.origin.x+own.frame.size.width+40, 10, 15, 20)];
+    UILabel *rest = [[UILabel alloc] initWithFrame:CGRectMake(own.frame.origin.x+own.frame.size.width+10, 10, 100, 20)];
     rest.text =@"残り日数";
+   
     [containerView addSubview:maker];
     [containerView addSubview:software];
     [containerView addSubview:version];
     [containerView addSubview:own];
     [containerView addSubview:rest];
+
     return containerView;
+    /*
+    UILabel* header = [[UILabel alloc] initWithFrame:CGRectMake(30, 0, 320, 64)];
+    header.text = @"メーカー　　　　";
+    header.backgroundColor = [UIColor whiteColor];
+    return header;
+     */
 }
+
+
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     int count = 0;
@@ -99,7 +114,14 @@
 {
     return 100;
 }
- 
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row % 2 == 0) {
+        cell.backgroundColor = [UIColor colorWithHue:0.61 saturation:0.09 brightness:0.99 alpha:1.0];
+    } else
+    cell.backgroundColor = [UIColor colorWithHue:0.99 saturation:0.09 brightness:0.99 alpha:1.0];
+}
 -(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     NSString *cellIdentifier = @"myCell2";
