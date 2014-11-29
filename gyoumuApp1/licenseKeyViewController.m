83fbd48b309bee26d34644d4619c1f5230a44a1c
Shota Oda <odendenden0606@gmail.com>
2014-11-29 18:43:43 +0900

    識別名とライセンスキー入力のテキストフィールドの修正 #128 コードでテキストフィールドを生成し直して、幅を大きくした

--- gyoumuApp1/licenseKeyViewController.m
+++ gyoumuApp1/licenseKeyViewController.m
@@ -13,11 +13,34 @@
 
 @end
 
-@implementation licenseKeyViewController
+@implementation licenseKeyViewController{
+    UITextField *tagField;
+    UITextField *keyField;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    //識別子のtextField
+    tagField =
+    [[UITextField alloc] initWithFrame:CGRectMake(158, 177, 451, 60)];
+    tagField.borderStyle = UITextBorderStyleRoundedRect;
+    tagField.keyboardType = UIKeyboardTypeASCIICapable;
+    tagField.placeholder = @"識別名を入力してください";
+    tagField.autocapitalizationType =  UITextAutocapitalizationTypeNone;
+    tagField.clearButtonMode = UITextFieldViewModeAlways;
+    
+    //ライセンスキーのtextField
+    keyField =
+    [[UITextField alloc] initWithFrame:CGRectMake(158, 393, 451, 60)];
+    keyField.borderStyle = UITextBorderStyleRoundedRect;
+    keyField.keyboardType = UIKeyboardTypeASCIICapable;
+    keyField.placeholder = @"ライセンスキーを入力してください";
+    keyField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    keyField.clearButtonMode = UITextFieldViewModeAlways;
+    [self.view addSubview:tagField];
+    [self.view addSubview:keyField];
     // Do any additional setup after loading the view.
 }
 
@@ -25,8 +48,8 @@
     // ここにtextデータの処理
     
     // キーボードを閉じる
-    [self.tagField resignFirstResponder];
-    [self.keyField resignFirstResponder];
+    [tagField resignFirstResponder];
+    [keyField resignFirstResponder];
     
 }
 - (void)didReceiveMemoryWarning {
@@ -49,21 +72,21 @@
 
 - (IBAction)next:(id)sender {
 
-    if(self.tagField.text.length >= 32){
+    if(tagField.text.length >= 32){
         
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"識別名が長過ぎます。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
         return;
         
-    }else if(self.tagField.text.length == 0){
+    }else if(tagField.text.length == 0){
     
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"識別名が入力されていません。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
         return;
         
-    }else if([self havingInvalidPatternIn:self.tagField.text]){
+    }else if([self havingInvalidPatternIn:tagField.text]){
         
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"識別名に不正な文字を含んでいます。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
@@ -71,21 +94,21 @@
         return;
         
         
-    }else if(self.tagField.text.length >= 32){
+    }else if(tagField.text.length >= 32){
         
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"キーが長過ぎます。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
         return;
         
-    }else if(self.keyField.text.length == 0){
+    }else if(keyField.text.length == 0){
     
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"ライセンスキーが入力されていません。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
         return;
         
-    }else if([self havingInvalidPatternIn:self.keyField.text]){
+    }else if([self havingInvalidPatternIn:keyField.text]){
         
         UIAlertView *alert =
         [[UIAlertView alloc] initWithTitle:@"入力エラー" message:@"ライセンスキーに不正な文字を含んでいます。"                              delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
@@ -94,8 +117,8 @@
         
     }
     
-    self.addData.tag = self.tagField.text;
-    self.addData.key = self.keyField.text;
+    self.addData.tag = tagField.text;
+    self.addData.key = keyField.text;
     [self performSegueWithIdentifier:@"key" sender:self];
     
 }
